2df7d38bf8983ec83839a23828b76dc983efdbbd
Juki <jukibenghantachi@gmail.com>
2022-02-08 20:52:24 +0700

    feat: add details page

---
+++ src/pages/details/[id].tsx
@@ -0,0 +1,23 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { DetailsComponent } from '../../components';
+import axios from 'axios';
+
+export default function Details() {
+    let { query, isReady } = useRouter();
+    let [details, setDetails] = useState();
+
+    const fetch = async (url: string) => {
+        const res = await axios.get(url);
+        setDetails(res.data.data);
+    };
+
+    useEffect(() => {
+        // Use isReady if query is undefined
+        // https://github.com/vercel/next.js/discussions/11484#discussioncomment-356055
+        if (!isReady) return;
+        fetch(`https://api.jikan.moe/v4/anime/${query.id}`);
+    }, [isReady]);
+
+    return <div>{details ? <DetailsComponent data={details} /> : null}</div>;
+}
