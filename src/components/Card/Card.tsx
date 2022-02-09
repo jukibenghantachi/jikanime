6311e237dff11552447b637d608d09e8bfd5688a
Juki <jukibenghantachi@gmail.com>
2022-02-09 13:30:31 +0700

    chore: manga card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,7 +5,7 @@ export default function Card({ type, data }) {
     switch (type) {
         case 'New Anime':
             return (
-                <Link href={'details/' + data.mal_id} passHref>
+                <Link href={'details/anime/' + data.mal_id} passHref>
                     <div className="group w-32 cursor-pointer">
                         <Image
                             src={data.images.webp.large_image_url}
@@ -23,7 +23,7 @@ export default function Card({ type, data }) {
             );
         case 'Latest Updated':
             return (
-                <Link href={'details/' + data.entry.mal_id} passHref>
+                <Link href={'details/anime/' + data.entry.mal_id} passHref>
                     <div className="group w-32 cursor-pointer">
                         <Image
                             src={data.entry.images.webp.large_image_url}
@@ -35,6 +35,26 @@ export default function Card({ type, data }) {
                         <div className="group-hover:text-[#2c54a4]">
                             <p className="font-medium line-clamp-2">{data.entry.title}</p>
                             <p className="text-sm text-gray-500">{data.episodes[0].title}</p>
+                        </div>
+                    </div>
+                </Link>
+            );
+        case 'Manga':
+            return (
+                <Link href={'details/manga/' + data.mal_id} passHref>
+                    <div className="group w-32 cursor-pointer">
+                        <Image
+                            src={data.images.webp.large_image_url}
+                            alt={data.title}
+                            width={128}
+                            height={192}
+                            className="duration-50 rounded-md object-cover transition group-active:scale-95"
+                        />
+                        <div className="group-hover:text-[#2c54a4]">
+                            <p className="font-medium line-clamp-2">{data.title}</p>
+                            <p className="text-sm text-gray-500">
+                                {data.chapters ? 'Chapters ' + data.chapters : null}
+                            </p>
                         </div>
                     </div>
                 </Link>
