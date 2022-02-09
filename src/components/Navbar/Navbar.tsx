0bc00d0c3f0dbaa6f70e4372afb811e4e80c94ff
Juki <jukibenghantachi@gmail.com>
2022-02-09 10:59:49 +0700

    refactor: change font size

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -22,13 +22,13 @@ export default function Navbar() {
         <nav className="fixed top-0 z-10 flex w-full items-center justify-between border-b-[1px] bg-white py-2 px-12">
             <div className="flex items-center space-x-3">
                 <Image src={Logo} alt="Jikan Logo" width={40} height={40} />
-                <p className="text-sm">Jikanime</p>
+                <p className="text-sm font-medium">Jikanime</p>
             </div>
             <div>
                 <ul className="flex space-x-5">
                     {routerList.map((items, id) => (
                         <Link href={items.route} key={id} passHref>
-                            <li className="cursor-pointer text-sm font-medium hover:text-[#2c54a4]">
+                            <li className="cursor-pointer text-xs font-medium hover:text-[#2c54a4]">
                                 {items.name}
                             </li>
                         </Link>
